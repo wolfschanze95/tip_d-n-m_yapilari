b8c9b85147b88ba1ac71972e24a406c5f6f07148
wolfschanze95 <rlpatlgan.01@gmail.com>
2024-11-14 15:50:59 +0300

    Update example.py

--- example.py
+++ example.py
@@ -6,3 +6,16 @@ str(c) # 55
 bool(b) # True
 int(a) # 5
 float(d) # 0.5
+
+
+
+"HESAP MAKİNESİ"
+sayi1=input("lütfen değer girin=")
+sayi2=input("lütfen değer girin=")
+a=float(sayi1)
+b=float(sayi2)
+print("toplam",a+b)
+print("fark",a-b)
+print("çarpım",a*b)
+print("üssü",a**b)
+print("bölüm",a/b)
